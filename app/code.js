0e0ebb91a35691544a490e1e1a0e3505b56233db
Rehan Ahmad <rehan.ahmad@nokia.com>
2015-10-21 15:39:30 +0100

    Fix the indentation to read & understand the code

--- app/code.js
+++ app/code.js
@@ -3,21 +3,21 @@ o = {
 };
 
 element = document.createElement('div');
-setTimeout(function(){
+setTimeout(function () {
   document.querySelector('body').insertBefore(element, document.querySelector('button'));
   element.textContent = 0;
 }, 200)
 
-function addFive () {
+function addFive() {
   addhistory(5)
   element.innerHTML = parseInt(element.innerHTML) + 5;
 }
-function addhistory (Added) {
-document.
-querySelector('span:last-child').appendChild(
-document.
-createElement('span')
 
-)
-document.querySelector('span:last-child').textContent += ' + ' + Added;
+function addhistory(Added) {
+  document.
+  querySelector('span:last-child').appendChild(
+    document.createElement('span')
+
+  )
+  document.querySelector('span:last-child').textContent += ' + ' + Added;
 }
--- app/code2.js
+++ app/code2.js
@@ -1,20 +1,22 @@
-function reqListener () {
-   element.textContent = oldNumber + parseInt(10);
-   element2.remove()
+function reqListener() {
+  element.textContent = oldNumber + parseInt(10);
+  element2.remove()
 }
 
-function addTen () { 
+function addTen() {
   oldNumber = parseInt(element.textContent)
   addhistory(10)
-element2 = document.createElement('div');
+  element2 = document.createElement('div');
   element2.textContent = 'adding'
-document.querySelector('body').insertBefore(element2, document.querySelector('button'));
+  document.querySelector('body').insertBefore(element2, document.querySelector('button'));
 
-var oReq = new XMLHttpRequest();
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "http://www.httpbin.org/delay/1", true);oReq.send(); }
+  var oReq = new XMLHttpRequest();
+  oReq.addEventListener("load", reqListener);
+  oReq.open("GET", "http://www.httpbin.org/delay/1", true);
+  oReq.send();
+}
 
 element = document.createElement('div');
-setTimeout(function(){
-document.querySelector('body').insertBefore(element, document.querySelector('button'));
+setTimeout(function () {
+  document.querySelector('body').insertBefore(element, document.querySelector('button'));
 }, 500)
